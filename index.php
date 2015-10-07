e9a12aaae8e0d22dbc6035b30d35b0d842435b97
王万霖 <dgideas@outlook.com>
2015-10-07 09:48:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,25 @@
 <!DOCTYPE HTML>
 <HTML>
-<HEAD>
-<TITLE></TITLE>
-</HEAD>
-<BODY>
-</BODY>
+	<HEAD>
+		<TITLE>BaxLib-Microservice</TITLE>
+	</HEAD>
+	<BODY>
+		<?php
+			if(isset($_POST["content"]))
+			{
+				$fHandle=fopen("tmp.content",'a');
+					fwrite($fHandle,$_POST["content"]);
+					fclose($fHandle);
+				passthru("python main.py analyze tem.content");
+				$resHandle=fopen("res.log",'r');
+					fread($resHandle);
+					fclose($resHandle);
+				var_dump($resHandle);
+			}
+			else
+			{
+				print('{"status":"arg_error"}');
+			}
+		?>
+	</BODY>
 </HTML>
